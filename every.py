89ecf0fef122e18ac98b02405acb1b8499ab159f
ckey <coenkey3@gmail.com>
2024-01-31 10:38:43 -0600

    update

--- every.py
+++ every.py
@@ -1,27 +1,49 @@
+import sounddevice as sd
+import soundfile as sf
+import numpy as np
 from transformers import pipeline
 
 summarizer = pipeline("summarization", model="Falconsai/text_summarization")
 transcriber = pipeline(task="automatic-speech-recognition", model="openai/whisper-base")
 
-
+duration = 5
 t=""
-for i in range:
+fs = 44100
+
+
+print("starting")
+for i in range(1):#how many durations that loop goes through
+    print(i)
+
+   #record the lecture
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
+    sd.wait()
+    #print('done')
+    file_path = "recorded_audio.wav"  # Set the file path to save the recording
+    print(f"Saving recording to {file_path}...")
+    sf.write(file_path, myrecording, fs)
+    print("Recording saved.")
+
+    #gets recorded audio and does the transcription
     x = transcriber("recorded_audio.wav")
-    print(x)
-    print("done")
-    with open("newtext.txt", "w") as f:
-        f.write()
-    
-    with open("shortadmasMIT17.txt", "r") as f:
-        text = f.read()
-    print(text)
-    
-    t+=i
+    print(x["text"])
+   # print("done")
+
+    #puts transcription into a text file
+    t+=x["text"]#taking the text from x "the dictionary"
     with open("text.txt", "w") as f:
-        f.write()
+        f.write(t)
+    
+    print("done interation")
 
-x = summarizer(text, max_length=230, min_length=30, do_sample=False)
+    #summary
+with open("text.txt", "r") as f:
+    text = f.read()
+print(text)
+    
 
-print(x)
 
+#x = summarizer(text, max_length=230, min_length=30, do_sample=False)
+
+#print(x)
 print("done")
