1dc37779a5d9def8bce45c4e5c55728e7ff2e344
Blas-U <cheezypizzagamer@gmail.com>
2024-01-31 09:37:43 -0600

    Yo yo yo

--- audiorecord-test.py
+++ audiorecord-test.py
@@ -24,3 +24,5 @@ file_path = "recorded_audio.wav"  # Set the file path to save the recording
 print(f"Saving recording to {file_path}...")
 sf.write(file_path, myrecording, fs)
 print("Recording saved.")
+
+yo yo yo 
