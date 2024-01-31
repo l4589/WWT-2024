b8c2b4ce3ef52c8e635140b1613a79f2800cc1a9
ckey <coenkey3@gmail.com>
2024-01-31 09:35:31 -0600

    update

--- summarize.py
+++ summarize.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 summarizer = pipeline("summarization", model="Falconsai/text_summarization")
 
 
-with open("adamsMIT17.txt", "r") as f:
+with open("shortadmasMIT17.txt", "r") as f:
     text = f.read()
 print(text)
 
