4a6689fb19cc29d72076f07eacd9f1bd11825903
Blas-U <cheezypizzagamer@gmail.com>
2024-02-18 15:18:28 -0600

    Added Grade Level Button

--- webpage/cTransOpenAI.py
+++ webpage/cTransOpenAI.py
@@ -9,6 +9,7 @@ def customTranscript(vData):
 
     if vData['bulletpoints']:
         bPoints = 'in bullet points'
+    gradeLevel = vData['gradeLevel']
 
     with open("AItext.txt", "r") as f:
         text = f.read()
@@ -16,7 +17,7 @@ def customTranscript(vData):
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a student studying, who returns data in HTML format."},
+        {"role": "system", "content": f"You are a {gradeLevel} grade student studying, who returns data in HTML format."},
         {"role": "user", "content": f"Summarize this lecture {bPoints} as HTML code: {text}"}
     ]
     )
