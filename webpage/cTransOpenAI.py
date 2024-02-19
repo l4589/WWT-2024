7294f245a9d99cf64c747ef004b19a02c3035897
Blas-U <cheezypizzagamer@gmail.com>
2024-02-19 13:21:53 -0600

    update

--- webpage/cTransOpenAI.py
+++ webpage/cTransOpenAI.py
@@ -6,9 +6,15 @@ def customTranscript(vData):
     client = OpenAI()
 
     print('custom transcript', vData)
-
+    if vData["SimpleVocab"]:
+        sVocab = 'and simple words,'
+    else: 
+        sVocab = ' '
     if vData['bulletpoints']:
         bPoints = 'in bullet points'
+    else:
+        bPoints = ' '
+        
     gradeLevel = vData['gradeLevel']
 
     with open("AItext.txt", "r") as f:
@@ -18,7 +24,7 @@ def customTranscript(vData):
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": f"You are a {gradeLevel} grade student studying, who returns data in HTML format."},
-        {"role": "user", "content": f"Summarize this lecture {bPoints} as HTML code: {text}"}
+        {"role": "user", "content": f"Summarize this lecture {bPoints} {sVocab} as HTML code: {text}"}
     ]
     )
     print(completion.choices[0].message)
